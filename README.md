ce82e4d5710c14d3c162dc3b0f886df92d22bd39
Rohan <rsharma7_be22@thapar.edu>
2026-05-12 17:54:11 +0530

    chore:updated .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # skrible
+# skrible
--- dist/services/authService.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function register(email: string, password: string): Promise<string>;
-export declare function login(email: string, password: string): Promise<string>;
-export declare function verifyToken(token: string): {
-    id: string;
-    email: string;
-};
-//# sourceMappingURL=authService.d.ts.map
